85a0c10573bdaac18a738f1055151a51be733685
Rohit Raj <irohit427@gmail.com>
2020-05-20 10:04:05 +0530

    Rearranged server

--- src/server.js
+++ src/server.js
@@ -9,41 +9,39 @@ const hpp = require('hpp');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const path = require('path');
+const mongoConnect = require('./config/db');
+const errorHandler = require('./middleware/error');
+const app = express();
+const PORT = process.env.PORT;
+const trip = require('./routes/trip');
+const auth = require('./routes/auth');
+dotenv.config({path: path.resolve(__dirname, './config/config.env')});
+
+mongoConnect();
+
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
     max: 1000
 });
 
-// Load env
-dotenv.config({path: path.resolve(__dirname, './config/config.env')});
-const mongoConnect = require('./config/db');
-const trip = require('./routes/trip');
-const auth = require('./routes/auth');
-const errorHandler = require('./middleware/error');
-//const logger = require('./middleware/logger');
-
-const app = express();
-const PORT = process.env.PORT;
-mongoConnect();
 app.use(morgan('combined'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(limiter);
 app.use(cors());
-app.use('/api/v1/trips', trip);
-app.use('/api/v1/auth', auth);
 app.use(errorHandler);
 app.use(mongoSanitize());
 app.use(helmet());
 app.use(xss());
 app.use(hpp());
+app.use('/api/v1/trips', trip);
+app.use('/api/v1/auth', auth);
 
 app.get('/', (req, res) => {
     res.send('Trips');
 })
 
-const server = app.listen(PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
 
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`);
